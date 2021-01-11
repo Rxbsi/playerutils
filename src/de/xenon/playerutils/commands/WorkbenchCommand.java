102884d7b43707273f605e732129f31406b2c025
Verronix <mauricelabonte2@gmail.com>
2021-01-11 18:44:49 +0100

    "second commit"

--- src/de/xenon/playerutils/commands/WorkbenchCommand.java
+++ src/de/xenon/playerutils/commands/WorkbenchCommand.java
@@ -1,11 +1,9 @@
 package de.xenon.playerutils.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 
 public class WorkbenchCommand implements CommandExecutor {
 
