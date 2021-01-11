a7bd966f9a1a8aeaaea6cf311a58e00c342084b2
Robinn <robinfranz02@gmail.com>
2021-01-11 15:00:32 +0100

    "second commit"

---
+++ src/de/xenon/playerutils/commands/PlayerCommand.java
@@ -0,0 +1,25 @@
+package de.xenon.playerutils.commands;
+
+import de.xenon.playerutils.Plugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class PlayerCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if(!(sender instanceof Player))
+            return false;
+
+        final Player player = (Player) sender;
+
+        if(player.hasPermission("playerutils.player") || player.hasPermission("playerutils.*")) {
+            player.sendMessage(Plugin.PREFIX + "Nigger");
+        }
+
+        return false;
+    }
+}
